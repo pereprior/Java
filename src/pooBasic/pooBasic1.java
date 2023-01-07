8a5b699dd39a82d72731edd27624cd6a8c904719
pprior <alupri5332@ieselcaminas.org>
2023-01-08 00:08:52 +0100

    07-01-23

--- src/pooBasic/pooBasic1.java
+++ src/pooBasic/pooBasic1.java
@@ -1,4 +1,20 @@
 package pooBasic;
 public class pooBasic1 {
 
+    private static final coche kia = new coche();
+
+    public static void cambiarColor() {
+
+        kia.setColor("black");
+
+        System.out.println(kia.getColor());
+
+    }
+
+    public static void main(String[] args) {
+
+        cambiarColor();
+
+    }
+
 }
